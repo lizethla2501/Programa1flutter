5936c585ec59cc480a5d59852fa237326e63c9ca
lizethla2501 <llizlopez578@gmail.com>
2025-04-29 12:21:27 -0600

    actualizacion

---
+++ practica1/lib/widget/Pagina1.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Pagina1 extends StatelessWidget {
+  const Pagina1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'HELLLOOOOUUU WORLD',
+        ),
+        backgroundColor: Colors.amber,
+      ),
+    );
+  }
+}
